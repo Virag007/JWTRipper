cdd1ac1e717de2ceb8089baf2eb6b257f30528b8
Virag007 <root@kali>
2020-12-17 19:33:48 +0530

    Added multiple functionality

--- JWTRipper.py
+++ JWTRipper.py
@@ -4,7 +4,7 @@
 ## Author: Parag Thakur (aka Virag)
 ## Follow: @_virag007
 ## Licence: MIT
-## Copyright: Copyright 2020, JWTRipper
+## Copyright: Copyright 2020, Virag
 ## Title: JWT Encoder, Decoder & Brute-forcer
 ## About: CLI Tool
 ##############################################
@@ -14,6 +14,10 @@ import base64
 from colorama import Fore, Style
 from pyfiglet import Figlet
 import argparse
+import json
+
+
+wordlists = []
 
 #Defining the banner
 def banner():
@@ -30,57 +34,81 @@ def usage():
 		\nDescription: A command line tool for encoding, decoding and brute-forcing JSON Web Token(JWT).""", formatter_class = argparse.RawTextHelpFormatter, usage = 'use "%(prog)s --help" for more information')
 
 	parser.add_argument('-e', '--encode', action = 'store_true', help = 'Encode a JWT Token')
-	parser.add_argument('-d', '--decode', action = 'store_true', help = 'Decode a JWT Token')
-	parser.add_argument('-w', '--wordlist', action = 'store_true', help = 'Specify a wordlist for brute-forcing')
+	parser.add_argument('-d', '--decode', nargs = 1,  help = 'Decode a JWT Token')
+	parser.add_argument('--brute', action = 'store_true', help = 'Enable brute-force mode')
+	parser.add_argument('-w', '--wordlist', type = argparse.FileType('r'), help = 'Specify a wordlist for brute-forcing')
 	parser.add_argument('--version', action = 'version', version = '%(prog)s v1.0 (Beta)', help = 'Shows the version information and exit')
 	args = parser.parse_args()
 	return args
 
 #Encoding a JWT Token
-def encode():
-	print('Encoding in progress')
-	print('\nEnter JWT parts: ')
-	#header = input('Header: ')
-	#payload = input('Payload: ')
-	#secret = input('Secret: ')
-	wordlist = ['hacker', 'jwt', 'insecurity', 'pentesterlab', 'hacking']
-
-	for word in wordlist:
-		encoded_jwt = jwt.encode({"user": null}, word, algorithm='HS256').decode('utf-8')
-		print(encoded_jwt)
-
-		if(encoded_jwt == 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjpudWxsfQ.Tr0VvdP6rVBGBGuI_luxGCOaz6BbhC6IxRTlKOW8UjM'):
-			print('Found Secret: ' + word)
-			break
+def encode(payload, secret, algo, header):
+	payload = json.loads(payload)
+	header = json.loads(header)
+	
+	if(header != ''):
+		try:
+			encoded_jwt = jwt.encode(payload, secret, algorithm=algo, headers=header).decode('utf-8')
+		except:
+			print('Wrong algorithm provided.')
+	else:
+		try:
+			encoded_jwt = jwt.encode(payload, secret, algorithm=algo).decode('utf-8')
+		except:
+			print('Wrong algorithm provided.')
+	return encoded_jwt
 
 
 #Decoding a JWT Token
-def decode():
-	print('Decoding under progress')
-	print('\nEnter the JWT Token: ')
-	jwt_token = input()
-	jwt_parts = jwt_token.split('.')
+def decode(token):
+	jwt_parts = token.split('.')
 	jwt_parts.pop()
+	jwt_data = []
 
 	for base in jwt_parts:
 		try:
-			print(base64.b64decode(base).decode("utf-8"))
+			jwt_data.append(base64.b64decode(base).decode("utf-8"))
 		except:
 			try:
 				base = base + '='
-				print(base64.b64decode(base).decode("utf-8"))
+				jwt_data.append(base64.b64decode(base).decode("utf-8"))
 			except:
 				base = base + '=='
-				print(base64.b64decode(base).decode("utf-8"))
+				jwt_data.append(base64.b64decode(base).decode("utf-8"))
+
+	return jwt_data
 
 
 #Brute-forcing a JWT Token to know about secret key
 def brute_force():
-	print('Brute-forcing under progress')
+	jwt_token = input('Enter the JWT Token: ')
+	jwt_data = decode(jwt_token)
+	
+	for word in wordlists:
+		encoded_jwt = encode(jwt_data[1], word, 'HS256', jwt_data[0])
+		if(encoded_jwt == jwt_token):
+			print(Fore.RED)
+			print('\033[1m' + 'Found Secret: ' + '\033[0m', end = '')
+			print(Style.RESET_ALL, end = '')
+			print(Fore.CYAN, end = '')
+			print(word)
+			print(Style.RESET_ALL)
+			break
+
+
+#Display
+def display(jwt_data):
+	header = json.loads(jwt_data[0])
+	payload = json.loads(jwt_data[1])
+	print('Header:')
+	print(json.dumps(header, indent = 4))
+	print('\nPayload:')
+	print(json.dumps(payload, indent = 4))
 
 
 #Menu-driven program
 def menu():
+	global wordlists
 	print('1. Encode JWT')
 	print('2. Decode JWT')
 	print('3. Brute-force the secret')
@@ -90,12 +118,33 @@ def menu():
 		choice = int(input('\nEnter your choice: '))
 
 		if(choice == 1):
-			encode()
+			algorithm = input('Algorithm: ').upper()
+			payload = input('Payload: ')
+			secret = input('Secret Key: ')
+			print('Do you want to add headers (Yes/No): ', end = '')
+			option = input()
+
+			if(option.lower()[0] == 'y'):
+				header = input('Header: ')
+				print('\nJWT Token: ' + encode(payload, secret, algorithm, header))
+			elif(option.lower()[0] == 'n'):
+				print('\nJWT Token: ' + encode(payload, secret, algorithm, ''))
+			else:
+				print('Enter yes or no only.')
 			
 		elif(choice == 2):
-			decode()
+			jwt_token = input('\nEnter the JWT Token: ')
+			display(decode(jwt_token))
 
 		elif(choice == 3):
+			wordlist_path = input('Enter the location of wordlist: ')
+			f = open(wordlist_path, 'r')
+			keys = f.read()
+			f.close()
+
+			keys = keys.split('\n')
+			keys.pop()
+			wordlists = keys
 			brute_force()
 
 		elif(choice == 4):
@@ -107,20 +156,43 @@ def menu():
 	except ValueError:
 		print('Enter an Integer option.')
 
+	except KeyboardInterrupt:
+		print('\nForced Exit\nGood Day')
+
 
 #Main function
 def main():
+	global wordlists
 	args = usage()
 	if args.encode:
-		print('Encode a JWT Token')
+		encode()
 		return
 
 	elif(args.decode):
-		print('Decode a JWT Token')
+		display(decode(args.decode[0]))
 		return
+
+	elif(args.brute):
+		if(args.wordlist):
+			for word in args.wordlist:
+				wordlists.append(word.strip('\n'))
+			
+			brute_force()
+			return
+		else:
+			print('Insufficient wordlist.')
+			return
+
 	elif(args.wordlist):
-		print('Specify a wordlist')
-		return
+		for word in args.wordlist:
+			wordlists.append(word.strip('\n'))
+
+		if(args.brute):
+			brute_force()
+			return
+		else:
+			print('Missing --brute option.')
+			return
 
 	banner()
 	menu()
